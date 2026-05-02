f31c54097e5e25afd4f4de34e77afb1d3263318e
boodyxdgamer <boodyehab2006@gmail.com>
2026-05-02 23:28:18 +0300

    Update PCB.java

--- OS_Project/src/main/java/com/mycompany/os_project/PCB.java
+++ OS_Project/src/main/java/com/mycompany/os_project/PCB.java
@@ -1,8 +1,28 @@
 package com.mycompany.os_project;
 
 public class PCB {
+    
+    //Attributes
+    int PID;
+    double AT;
+    double BT;
+    double RTM;
+    double CT;
+    double TAT;
+    double WT;
+    double RT;
 
-    public PCB() {
+    public PCB(int PID, double AT, double BT, double RTM, double CT, double TAT, double WT, double RT) {
+        this.PID = PID;
+        this.AT = AT;
+        this.BT = BT;
+        this.RTM = RTM;
+        this.CT = CT;
+        this.TAT = TAT;
+        this.WT = WT;
+        this.RT = RT;
     }
+
+   
     
 }
