5e9a11e1cd6de80af8e34ccf40d536d0b2e8eb43
boodyxdgamer <boodyehab2006@gmail.com>
2026-05-02 23:06:59 +0300

    first commit

---
+++ OS_Project/src/main/java/com/mycompany/os_project/OS_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.os_project;
+
+/**
+ *
+ * @author Abdelrahman
+ */
+public class OS_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
