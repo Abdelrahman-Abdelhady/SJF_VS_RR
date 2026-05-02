c24ac5d7adf8e31d7c335406f62164cee87814a0
boodyxdgamer <boodyehab2006@gmail.com>
2026-05-03 00:36:01 +0300

    finished PCB v1

--- OS_Project/src/main/java/com/mycompany/os_project/PCB.java
+++ OS_Project/src/main/java/com/mycompany/os_project/PCB.java
@@ -6,11 +6,17 @@ public class PCB {
     int PID;
     double AT;
     double BT;
-    double RTM;
-    double CT;  // Abdelrahman (the cpu clock value when the proccess hhave completed)
+    
+    //should be calculated whenevever the Proccess enters Excution
+    double RTM; //Remaining excution time for round robin
+    
+    //should be calulated when the proccess Finishes excution
+    double CT;  // Abdelrahman (the CPU clock value when the proccess have completed)
+    //should be calulated when the proccess Finishes excution
     double TAT; // Abdelghani (total time the process spent in the system, from arrival to completion.)
-    double WT;  // Abdelghani
-    double RT;  // Abdelrahman (Time from the arrival till the proccess is first excuted by the CPU)
+    double WT;  // Abdelghani (The total time a process spends waiting in the ready queue before and between CPU bursts.)
+    //should be calulated when the proccess starts excution
+    double RT;  // Abdelrahman (Time from the arrival till the proccess is first excuted by the CPU) 
 
     public PCB(int PID, double AT, double BT, double RTM, double CT, double TAT, double WT, double RT) {
         this.PID = PID;
@@ -42,5 +48,25 @@ public class PCB {
         this.RT = firstStartTime - this.AT;
     }
     
+    //Update RTM
+     void updateRTM(double quantumTime)
+    {
+        this.RTM -= quantumTime;
+    }
+    
+    
+    
+    // Method that Calculates the Total time in system 
+    void calculateTAT()
+    {
+        this.TAT = this.CT - this.AT ;
+    }  
+    
+    // Method that Calculates the Completion time
+    void calculateWT()
+    {
+        this.WT = this.TAT - this.BT ;
+    }  
+     
     
 }
