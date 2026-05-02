5cb30fb1c50520d69221bee2ee21cef8435eaa82
boodyxdgamer <boodyehab2006@gmail.com>
2026-05-02 23:40:35 +0300

    Update OS_Project.java

--- OS_Project/src/main/java/com/mycompany/os_project/OS_Project.java
+++ OS_Project/src/main/java/com/mycompany/os_project/OS_Project.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.os_project;
-
+import java.util.*;
 /**
  *
  * @author kolenaaa
@@ -11,6 +11,16 @@ package com.mycompany.os_project;
 public class OS_Project {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Yala nebd2 el projectttt!!!");
+        
+        List<PCB> processes = Arrays.asList(
+    new PCB(1, 0, 5, 5, 0, 0, 0, 0),
+    new PCB(2, 1, 3, 3, 0, 0, 0, 0),
+    new PCB(3, 2, 8, 8, 0, 0, 0, 0),
+    new PCB(4, 3, 6, 6, 0, 0, 0, 0)
+    );
+
+    Queue<PCB> readyQueue = new LinkedList<>(processes);
+
     }
 }
