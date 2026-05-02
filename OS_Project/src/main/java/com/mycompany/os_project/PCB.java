32e0b569be6f4a4db9a7e5b94750e25a2491ddcf
boodyxdgamer <boodyehab2006@gmail.com>
2026-05-02 23:47:06 +0300

    Update PCB.java

--- OS_Project/src/main/java/com/mycompany/os_project/PCB.java
+++ OS_Project/src/main/java/com/mycompany/os_project/PCB.java
@@ -7,10 +7,10 @@ public class PCB {
     double AT;
     double BT;
     double RTM;
-    double CT;
-    double TAT;
-    double WT;
-    double RT;
+    double CT;  // abdelrahman
+    double TAT; // Abdelghani
+    double WT;  // Abdelghani
+    double RT;  // abdelrahman
 
     public PCB(int PID, double AT, double BT, double RTM, double CT, double TAT, double WT, double RT) {
         this.PID = PID;
