d633d5da9956ac048d330d8c6a3514db1d10f440
hend-eng <hendhussein2006@gmail.com>
2026-05-04 12:04:04 +0300

    gui1

---
+++ OS_Project/src/main/java/com/mycompany/os_project/SchedulerUtils.java
@@ -0,0 +1,15 @@
+package com.mycompany.os_project;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class SchedulerUtils {
+    public static Queue<PCB> deepCopyQueue(Queue<PCB> processes) {
+        Queue<PCB> copy = new LinkedList<>();
+        for (PCB p : processes) {
+            copy.add(new PCB(p)); 
+        }
+        return copy;
+    }
+}
---
+++ OS_Project/src/main/java/com/mycompany/os_project/SjfScheduler.java
@@ -0,0 +1,99 @@
+package com.mycompany.os_project;
+
+import java.util.*;
+/**
+ *
+ * @author Rodina
+ */
+public class SjfScheduler {
+
+    int clock = 0;
+    List<String> ganttChart = new LinkedList<>();
+    Queue<PCB> executed = new LinkedList<>();
+
+    public void execute(Queue<PCB> processesQueue) {
+        Queue<PCB> Copy_processes = new LinkedList<>(SchedulerUtils.deepCopyQueue(processesQueue));
+        List<PCB> allProcesses = new ArrayList<>(processesQueue);
+
+        int n = allProcesses.size();
+        int completed = 0;
+        PCB currentProcess = null;
+
+        while (completed < n) {
+            List<PCB> readyQueue = new ArrayList<>();
+            for (PCB p : allProcesses) {
+                if (p.AT <= clock && p.RTM > 0) {
+                    readyQueue.add(p);
+                }
+            }
+            System.out.println("\nProcess Queue:");
+            System.out.println(readyQueue.toString());
+
+            PCB shortest = null;
+            double minRemainingTime = Double.MAX_VALUE;
+
+            for (PCB p : allProcesses) {
+                if (p.AT <= clock && p.RTM > 0) {
+                    if (p.RTM < minRemainingTime) {
+                        minRemainingTime = p.RTM;
+                        shortest = p;
+                    }
+                }
+            }
+            if (shortest == null) {
+                clock++;
+                continue;
+            }
+            System.out.println("\nExecuting: " + shortest.toString());
+            if (currentProcess == null || currentProcess.PID != shortest.PID) {
+                ganttChart.add(clock + "-P" + shortest.PID);
+                currentProcess = shortest;
+            }
+            shortest.RTM--;
+            clock++;
+            if (shortest.RTM == 0) {
+                completed++;
+                shortest.FinishProcess(clock);
+                executed.add(shortest);
+                currentProcess = null;
+                System.out.println(">>> Process P" + shortest.PID + " finished at Time " + clock);
+            }
+        }
+        String finalGantt = String.join("-", ganttChart) + "-" + clock;
+        System.out.println("\nFinal Gantt Chart: [" + finalGantt + "]");
+
+        AverageWaitingTime(executed);
+        AverageTAT(executed);
+        AverageRT(executed);
+
+        //return executed; 
+    }
+
+    public void AverageWaitingTime(Queue<PCB> finishedProcesses) {
+        double totalWaitingTime = 0;
+
+        for (PCB p : finishedProcesses) {
+            totalWaitingTime += p.WT;
+        }
+        System.out.println("Average Waiting Time -SJF- = " + totalWaitingTime / finishedProcesses.size());
+    }
+
+    public void AverageTAT(Queue<PCB> finishedProcesses) {
+        double totalTAT = 0;
+
+        for (PCB p : finishedProcesses) {
+            totalTAT += p.TAT;
+        }
+        System.out.println("Average TAT -SJF- = " + totalTAT / finishedProcesses.size());
+    }
+
+    public void AverageRT(Queue<PCB> finishedProcesses) {
+        double totalRT = 0;
+
+        for (PCB p : finishedProcesses) {
+            totalRT += p.RT;
+        }
+        System.out.println("Average RT -SJF- = " + totalRT / finishedProcesses.size());
+    }
+}
+
